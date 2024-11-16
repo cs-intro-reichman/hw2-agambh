f514f453fb7ac0d8e43d271f7204b39b8896df52
agambh <agam.haim@post.runi.ac.il>
2024-11-16 14:49:13 +0200

    test 1

--- TimeCalc.java
+++ TimeCalc.java
@@ -1,5 +1,10 @@
 public class TimeCalc {
     public static void main(String[] args) {
-        // Replace this comment with your code
+        int hour=Integer.parse(arg[0].)
+        int minutes=
+        int new=
+        int addH=
+        int addM=
+
     }
 }
